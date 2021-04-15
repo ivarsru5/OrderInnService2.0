40c34259c9ef92b14da59d821fbeb69baf662d50
ivarsru5 <ivarsrugelis.rugelis@inbox.lv>
2021-04-15 09:20:06 +0300

    Initial Commit

---
+++ OrderInnService/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  OrderInnService
+//
+//  Created by Ivars Ruģelis on 15/04/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ OrderInnService/OrderInnServiceApp.swift
@@ -0,0 +1,17 @@
+//
+//  OrderInnServiceApp.swift
+//  OrderInnService
+//
+//  Created by Ivars Ruģelis on 15/04/2021.
+//
+
+import SwiftUI
+
+@main
+struct OrderInnServiceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
